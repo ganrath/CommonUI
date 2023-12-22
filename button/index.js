4834c6cda5a5d8597111fb4b320988a33a0e6521
BHARATH GANESAN <bharath.ganesan@in.imshealth.com>
2023-12-22 10:49:01 +0530

    first commit

---
+++ button/index.js
@@ -0,0 +1,8 @@
+import stringWidth from 'string-width'
+const text = 'ITR UI DEMO 2 TRDDSDFSD'
+const button = () => { 
+    console.log(stringWidth(text))
+     console.log(text.toUpperCase())
+}
+
+export default button
